35d84aeefe428bf7a3b63f68ef505d11daebc41c
holiukvalentyn <holiukvalentyn@gmail.com>
2019-12-24 18:17:13 +0200

    1 commit

---
+++ index_2.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perpetio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class="header"></div>
+<div class="menu"></div>
+<div class="content"></div>
+
+<!--<h3>hello</h3>-->
+
+<header class="page-header">
+    <div class="nav-container">
+        <nav class="top-bar overlay-bar">
+            <div class="container">
+
+                <div class="row nav-menu">
+                    <div class="col-sm-3 col-md-2 columns">
+                        <a href="https://perpet.io/">
+<!--                            <img class="logo logo-light" alt="Perpetio" src="img/logo-light.png" />-->
+                            <img class="logo logo-dark" alt="Perpetio" src="img/perpetio_logo1.png" />
+                        </a>
+                    </div>
+
+                    <div class="col-sm-9 col-md-10 columns">
+                        <ul class="menu">
+                            <li class="active">
+                                <a href="#home">Home</a>
+                            </li>
+                            <li class="active">
+                                <a href="#portfolio">Portfolio</a>
+                            </li>
+                            <li class="active">
+                                <a href="#careers">Careers</a>
+                            </li>
+                            <li class="active">
+                                <a href="https://medium.com/perpetio">BLOG</a>
+                            </li>
+                            <li class="active">
+                                <a href="#contact">CONTACT US</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--end of row-->
+
+                <div class="mobile-toggle">
+                    <i class="icon icon_menu"></i>
+                </div>
+
+            </div>
+            <!--end of container-->
+        </nav>
+
+    </div>
+</header>
+
+</body>
+
+    <footer>
+
+    </footer>
+
+</html>
