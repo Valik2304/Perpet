3685bebbf1624e4494db18baac4072996bee4924
holiukvalentyn <holiukvalentyn@gmail.com>
2020-01-06 16:12:28 +0200

    Перший коміт - спроба нової верстки

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perpetio 2</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font.css">
+</head>
+<body>
+<header class="header">
+    <div class="wrapper">
+
+       <div class="header__wrapper">
+           <div class="header__logo">
+               <a href="/" class="header__logo-link">
+                   <img src="/svg/perpetio_logo_SVG.svg" alt="Perpetio" class="header__logo-pic">
+               </a>
+           </div>
+
+
+           <nav class="header__nav">
+               <ul class="header__list">
+                   <li class="header__item">
+                       <a href="#!" class="header__link header__link-home">Home</a>
+                   </li>
+                   <li class="header__item">
+                       <a href="#!" class="header__link">Portfolio</a>
+                   </li>
+                   <li class="header__item">
+                       <a href="#!" class="header__link">Careers</a>
+                   </li>
+                   <li class="header__item">
+                       <a href="#!" class="header__link">Blog</a>
+                   </li>
+                   <li class="header__item">
+                       <a href="#!" class="header__link header__link-contact">Contact Us</a>
+                   </li>
+               </ul>
+           </nav>
+       </div>
+
+    </div>
+</header>
+
+<main class="main">
+    <section class="intro">
+
+    </section>
+</main>
+
+</body>
+
+    <footer>
+
+    </footer>
+
+</html>
